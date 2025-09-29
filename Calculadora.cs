63d6f29a91d53557b6e9473ba4ddf8e7a11110c1
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-09-29 09:25:09 -0300

    feat: Class calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,18 @@
+namespace MembrosEstaticos
+{
+    class Calculadora
+    {
+
+        public static double Pi = 3.14;
+
+        public static double Calculo(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+        }
+    }
+}
